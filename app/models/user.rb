cfe669c92106440af69643a4fa663b31df8c3334
cschlessinger <claire.schlessinger@gmail.com>
2016-02-13 13:41:19 -0800

    Clean up user/owner models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,28 @@
 class User < ActiveRecord::Base
 
+  def analyze
 
-  # def analyze
+		content = self.bio
+  		big_5 = {}
 
-		# content = self.bio
-  # 		big_5 = []
+  	  response = Excon.post(ENV['url'],
+  	    :body => content,
+  	    :headers => { "Content-Type" => "text/plain" },
+  	    :user => ENV['user'],
+  	    :password => ENV['pass']
+  	  )
+  	  
+  	  profile = JSON.load(response.body)
 
-  # 	  response = Excon.post(ENV['BASE_URL']),
-  # 	    :body => content,
-  # 	    :headers => { "Content-Type" => "text/plain" },
-  # 	    :user => ENV['USER'],
-  # 	    :password => ENV['PASS']
-  # 	  )
+  	  profile["tree"]["children"][0]["children"][0]["children"].each do |trait|
+  	  	big_5[trait["name"]] = trait["percentage"]
+  	  end
 
-  # 	  profile = JSON.load(response.body)
+  	  big_5.sort {|a,b| b[1]<=>a[1]}
+  	  			
+		end
 
-  # 	  profile["tree"]["children"][0]["children"][0]["children"].each do |trait|
-  # 	  	big_5 << trait["percentage"]
-  # 	  end
+		p big_5
 
-		# end
-
+  	end
 end
