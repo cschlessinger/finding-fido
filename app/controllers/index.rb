0796140e442b97fa6b9771daad82ecf127492ae0
cschlessinger <claire.schlessinger@gmail.com>
2016-02-13 20:41:28 -0800

    Parse data from petfinder

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,4 +9,7 @@
 get '/' do
 	@matches = User.first.match
 	erb :application
+	@matches.each do |breed|
+		breed = Dog.find(breed)
+	end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,12 +1,14 @@
 class Dog < ActiveRecord::Base
+  require 'awesome_print'
   include Analyzable
   after_create :analyze
-  require 'awesome_print'
 
   def self.find(breed)
-		response = Excon.get("http://api.petfinder.com/pet.find?location=san+francisco+CA&breed=#{breed}&format=json&count=1&key=#{ENV['petfinder_key']}")
-		p response[:body][:petfinder]
-		# ["pets"]["pet"][0]["age"]
+		response = Excon.get("http://api.petfinder.com/pet.find?location=san+francisco+CA&breed=#{breed}&format=json&count=3&key=#{ENV['petfinder_key']}")
+		body = JSON.load(response.body)
+		name = body["petfinder"]["pets"]["pet"]["name"]
+		description = body["petfinder"]["pets"]["pet"]["description"]
+		photo = body["petfinder"]["pets"]["pet"]["media"]["photos"]["photo"][0]["$t"]
 	end
 
 end
