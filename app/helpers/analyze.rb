bdd69db1df4685423251fcbf48034b4cbb4262d2
cschlessinger <claire.schlessinger@gmail.com>
2016-02-13 19:47:57 -0800

    PetFinder json parsing in progress

--- app/helpers/analyze.rb
+++ app/helpers/analyze.rb
@@ -7,17 +7,17 @@ module Analyzable
 		big_5 = {}
 		ranked_traits = []
 
-	  response = Excon.post(ENV['url'],
+	  response = Excon.post(ENV['ibm_url'],
 	    :body => content,
 	    :headers => { "Content-Type" => "text/plain" },
-	    :user => ENV['user'],
-	    :password => ENV['pass']
+	    :user => ENV['ibm_user'],
+	    :password => ENV['ibm_pass']
 	  )
 
 	  profile = JSON.load(response.body)
 
 	  profile["tree"]["children"][0]["children"][0]["children"].each do |trait|
-		  self.update(trait["id"].downcase => trait["percentage"] * 100)
+		  self.update(trait["id"].downcase => trait["percentage"] * 10)
 	  end
 
 	end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,12 @@
 class Dog < ActiveRecord::Base
   include Analyzable
   after_create :analyze
+  require 'awesome_print'
+
+  def self.find(breed)
+		response = Excon.get("http://api.petfinder.com/pet.find?location=san+francisco+CA&breed=#{breed}&format=json&count=1&key=#{ENV['petfinder_key']}")
+		p response[:body][:petfinder]
+		# ["pets"]["pet"][0]["age"]
+	end
 
 end
