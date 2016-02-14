27c4c16234cf1016cd708aa16059ee9d2ef28f31
Malicemalone <m.alice.malone@gmail.com>
2016-02-13 20:46:11 -0800

    adds seed data make changes to analyze algorithm

--- app/helpers/analyze.rb
+++ app/helpers/analyze.rb
@@ -6,20 +6,21 @@ module Analyzable
 
 		big_5 = {}
 		ranked_traits = []
+ 		if self.openness == nil || self.agreeableness == nil || self.neuroticism == nil || self.extraversion == nil || self.conscientiousness == nil
+		  response = Excon.post(ENV['url'],
+		    :body => content,
+		    :headers => { "Content-Type" => "text/plain" },
+		    :user => ENV['user'],
+		    :password => ENV['pass']
+		  )
 
-	  response = Excon.post(ENV['url'],
-	    :body => content,
-	    :headers => { "Content-Type" => "text/plain" },
-	    :user => ENV['user'],
-	    :password => ENV['pass']
-	  )
+		  profile = JSON.load(response.body)
 
-	  profile = JSON.load(response.body)
 
-	  profile["tree"]["children"][0]["children"][0]["children"].each do |trait|
-		  self.update(trait["id"].downcase => trait["percentage"] * 100)
-	  end
-
+		  profile["tree"]["children"][0]["children"][0]["children"].each do |trait|
+			  self.update(trait["id"].downcase => trait["percentage"] * 10)
+	  	end
+		end
 	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ def match
 breeds_array = []
 
   Dog.all.each do |pup|
-    if (self.openness + 15) >= pup.openness && pup.openness >= (self.openness - 15)
-      if (self.agreeableness + 15) >= pup.agreeableness && pup.agreeableness >= (self.agreeableness - 15)
-        if (self.extraversion + 15) >= pup.extraversion && pup.extraversion >= (self.extraversion - 15)
-          if (self.conscientiousness + 15) >= pup.conscientiousness && pup.conscientiousness >= (self.conscientiousness - 15)
-            if (self.neuroticism + 15) >= pup.neuroticism && pup.neuroticism >= (self.neuroticism - 15)
+    if (self.openness + 3) >= pup.openness && pup.openness >= (self.openness - 3)
+      if (self.agreeableness + 3) >= pup.agreeableness && pup.agreeableness >= (self.agreeableness - 3)
+        if (self.extraversion + 3) >= pup.extraversion && pup.extraversion >= (self.extraversion - 3)
+          if (self.conscientiousness + 3) >= pup.conscientiousness && pup.conscientiousness >= (self.conscientiousness - 3)
+            if (self.neuroticism + 3) >= pup.neuroticism && pup.neuroticism >= (self.neuroticism - 3)
               breeds_array << pup.breed
             end
           end
