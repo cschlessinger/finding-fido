a64a99217c8f438da58bbbcacb2a3003262a9bdd
cschlessinger <claire.schlessinger@gmail.com>
2016-02-13 18:05:21 -0800

    Reorganize front-end files

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,13 +1,13 @@
-get '/dogs/:id' do
-	dog = Dog.find(params[:id])
-	dog.analyze
-end
+# get '/dogs/:id' do
+# 	dog = Dog.find(params[:id])
+# 	dog.analyze
+# end
 
-get '/owners/:id' do
-	owner = Owner.find(params[:id])
-	owner.analyze
-end
+# get '/owners/:id' do
+# 	owner = Owner.find(params[:id])
+# 	owner.analyze
+# end
 
 get '/' do
-	p "*" * 100
+	erb :application
 end
--- public/js/application.js
+++ public/js/application.js
@@ -11,6 +11,6 @@ $(document).ready(function() {
  //= require_tree .
 
   $(function() {
-    		$( "#tabs" ).tabs();
- 		});
+    $("#tabs").tabs();
+ 	});
 });
