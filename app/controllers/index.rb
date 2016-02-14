a3ce2f05571616bcff45b6e6ba23c45d0a29d5fc
cschlessinger <claire.schlessinger@gmail.com>
2016-02-13 22:09:03 -0800

    Successfully pass data from API call to view

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,8 +8,29 @@
 
 get '/' do
 	@matches = User.first.match
+
+	# @matches.each do |breed|
+	# 	breed = Dog.find(breed)
+	# end
+	@dogs = Dog.search("pug")
+
+
+	# p @dogs["31947859"]
+
+	# @dogs.each do |key|
+	# 	p key
+	# end
+
+	# thing.each do |key, value|
+	# 	p key[value]
+	# end
+
+
+
+	# @matches.each do |breed|
+	# 	@breed = Dog.search(breed)
+	# 	p @breed
+	# end
+
 	erb :application
-	@matches.each do |breed|
-		breed = Dog.find(breed)
-	end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,12 +3,35 @@ class Dog < ActiveRecord::Base
   include Analyzable
   after_create :analyze
 
-  def self.find(breed)
-		response = Excon.get("http://api.petfinder.com/pet.find?location=san+francisco+CA&breed=#{breed}&format=json&count=3&key=#{ENV['petfinder_key']}")
+  def self.search(breed)
+		response = Excon.get("http://api.petfinder.com/pet.find?location=san+francisco+CA&breed=#{breed}&format=json&count=100&key=#{ENV['petfinder_key']}")
 		body = JSON.load(response.body)
-		name = body["petfinder"]["pets"]["pet"]["name"]
-		description = body["petfinder"]["pets"]["pet"]["description"]
-		photo = body["petfinder"]["pets"]["pet"]["media"]["photos"]["photo"][0]["$t"]
+
+		# p body["petfinder"]["pets"]["pet"]["name"]["$t"]
+
+  # 	data = {}
+
+		# body["petfinder"]["pets"]["pet"].each do |dog|
+		# 	id = dog["id"]["$t"]
+		# 	data[id] = {}
+		# 	data[id]["name"] = dog["name"]["$t"]
+		# 	data[id]["description"] = dog["description"]["$t"]
+		# 	data[id]["photo"] = dog["media"]["photos"]["photo"][0]["$t"]
+		# end
+
+  	data = []
+
+		body["petfinder"]["pets"]["pet"].each do |dog|
+			arr = [dog["name"]["$t"], dog["description"]["$t"], dog["media"]["photos"]["photo"][0]["$t"]]
+			data << arr
+		end
+
+		data
+
+		# ["name"]
+		# data["description"] = body["petfinder"]["pets"]["pet"]["description"]
+		# data["photo"] = body["petfinder"]["pets"]["pet"]["media"]["photos"]["photo"][0]["$t"]
+		# p data
 	end
 
 end
